57996f8b265438f4c6290c5e33d525c6e05405ac
ad <dyernisus@gmail.com>
2015-01-24 23:00:08 -0500

    updated main EHT

--- Blocks/src/BlocksViewer.java
+++ Blocks/src/BlocksViewer.java
@@ -1,19 +1,26 @@
+import java.awt.EventQueue;
+
 import javax.swing.JFrame;
 
 public class BlocksViewer 
 {
 	public static void main(String[] args)
 	{
+		
+		EventQueue.invokeLater(new Runnable() {
+
+			public void run() {
 		JFrame frame = new JFrame();
 		
 		frame.setSize(600, 600);
 		frame.setTitle("Blocks");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		Component_01__Blocks_01 component = new Component_01__Blocks_01();
-		frame.add(component);
+		//Component_01__Blocks_01 component = new Component_01__Blocks_01();
+		frame.add(new Component_01__Blocks_01());
 		
 		frame.setVisible(true);
 	}
+		});
 
-}
+}}
